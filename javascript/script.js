7566166ac2f805be640ad25dab8d3a2d3354acd3
Glitch (lizard-fluff-pearl) <none>
2021-02-15 15:22:55 +0000

    Exporting to Opware2000/Evaluation4html ./javascript/script.js:28623938/105 ./xhtml.html:28623497/9633:28623938/3

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,12 @@
 function changer_cote_float()
 {
-  var css = document.getElementById(‘imageflottante′).style.float;
-  if css == 
-  =’right';
+  var css = document.getElementById('imageflottante').style.float;
+  if (css == 'right')
+    {
+      css = 'left';
+    }
+  else
+    {
+      css ='right';
+    }
 }
