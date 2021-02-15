af3ad0e938290bb4e238b1aabd13420e5349d019
Glitch (lizard-fluff-pearl) <none>
2021-02-15 17:02:10 +0000

    ⭐️🚵 Checkpoint ./javascript/script.js:28623497/2890 ./xhtml.html:28623497/3096:28623938/89 ./styles/styles.css:28623497/32

--- javascript/script.js
+++ javascript/script.js
@@ -5,48 +5,46 @@
 /*  ---------------------------------------------- */
 
 /* Fonction de changement de côté de l'image de Georges Boole et du bouton */
-function changer_cote_float()
-{
-  var div = document.getElementById('imageflottante');
-  if (div.style.float == 'right')
-    {
-      div.style.float = 'left';
-      div.style.marginLeft = "0";
-      div.style.marginRight= "10px";
-    }
-  else
-    {
-      div.style.float ='right';
-      div.style.marginLeft = "10px";
-      div.style.marginRight= "0px";
-    }
+function changer_cote_float() {
+  var div = document.getElementById("imageflottante");
+  if (div.style.float == "right") {
+    div.style.float = "left";
+    div.style.marginLeft = "0";
+    div.style.marginRight = "10px";
+  } else {
+    div.style.float = "right";
+    div.style.marginLeft = "10px";
+    div.style.marginRight = "0px";
+  }
 }
 
 /* Fonction qui affiche l'heure machine */
-function heure_machine()
-{
-  var item = document.getElementById('heure');
-  var now = new Date()
-  var h = now.getHours(), m = now.getMinutes(), s = now.getSeconds();
-  if (h<10)
-  {
+function heure_machine() {
+  var item = document.getElementById("heure");
+  var now = new Date();
+  var h = now.getHours(),
+    m = now.getMinutes(),
+    s = now.getSeconds();
+  if (h < 10) {
     h = "0" + h;
   }
-  if (m<10)
-  {
+  if (m < 10) {
     m = "0" + m;
   }
-  if (s<10)
-  {
+  if (s < 10) {
     s = "0" + s;
   }
   item.innerHTML = "Heure machine " + h + ":" + m + ":" + s;
-  refresh();
+  rafraichir();
+}
+
+function rafraichir() {
+  var t = 1000; // rafraîchissement en millisecondes
+  setTimeout("heure_machine()", t);
 }
 
 /* Fonction d'initialisation de la page : formulaire + heure */
-function init()
-{
+function init() {
   heure_machine();
 }
 
