a276b2acd4d3769803db0dff55d0b35f768b735c
Nicolas Ogier <nicolas.ogier@gmail.com>
2021-02-16 00:28:54 +0100

    Début fonction réinitialiser mise en forme du code javascript

--- javascript/script.js
+++ javascript/script.js
@@ -45,41 +45,94 @@ function rafraichir() {
 
 /* Création de l'exercice */
 
-function creer_exercice(){
-  var valeurA = document.getElementById('valeurA') ;
-  var valeurB = document.getElementById('valeurB') ;
+function creer_exercice() {
+  /* Zones d'affichage */
+  var valeurA = document.getElementById('valeurA');
+  var valeurB = document.getElementById('valeurB');
   var nomFonction = document.getElementById('valeur_fonction_logique');
   /* Récupère les boutons radio */
   var a0 = document.getElementById('a0');
   var a1 = document.getElementById('a1');
   var b0 = document.getElementById('b0');
   var b1 = document.getElementById('b1');
-  
-  var a, b ;
+  /* Récupère le nom de la fonction affichée dans le menu déroulant */
+  /* TODO */
+
+  /* Récupère les cases à cocher */
+
+
+  var a, b;
   /* Vérification*/
-  if (a0.checked){
+  if (a0.checked) {
     a = a0.value;
   }
-   if (a1.checked){
+  if (a1.checked) {
     a = a1.value;
   }
-   if (b0.checked){
+  if (b0.checked) {
     b = b0.value;
   }
-   if (b1.checked){
+  if (b1.checked) {
     b = b1.value;
   }
-  
-  valeurA.innerHTML=' vaut '+a;
-  valeurB.innerHTML=' vaut '+b;
-  console.log(a,b)
-  
+  if (a == 1) {
+    a = 'VRAI';
+  } else {
+    a = 'FAUX';
+  }
+  if (b == 1) {
+    b = 'VRAI';
+  } else {
+    b = 'FAUX';
+  }
+  valeurA.innerHTML = ' est ' + a;
+  valeurB.innerHTML = ' est ' + b;
+  console.log(a, b)
+
+}
+/* Menu déroulant */
+function change_valeur() {
+  var select = document.getElementById("fonctionLogique");
+  var choice = select.selectedIndex;
+  var valeur = select.options[choice].value;
+  var texte = select.options[choice].text;
+  var nomFonction = document.getElementById('valeur_fonction_logique');
+  if (valeur != '0') {
+    nomFonction.innerHTML = 'Fonction logique <span class="gras">' + texte.toUpperCase() + "</span>";
+  } else {
+    nomFonction.innerHTML = '<span class="gras">Choisir une fonction logique</span>';
+  }
 }
 
 /* Fonction d'initialisation de la page : formulaires + heure */
 function init() {
   heure_machine();
+  raz();
 }
 
-init();
+/* Fonction de remise à zéro du formulaire */
+function raz() {
+  /* Menu déroulant */
+  var select = document.getElementById("fonctionLogique");
+  select.value = 0;
+  /* Boutons radio */
+  var a0 = document.getElementById('a0');
+  var a1 = document.getElementById('a1');
+  var b0 = document.getElementById('b0');
+  var b1 = document.getElementById('b1');
+  a0.checked = true;
+  a1.checked = false;
+  b0.checked = true;
+  b1.checked = false;
+  /* Case à cocher */
 
+  /* Zones d'affichage */
+  var valeurA = document.getElementById('valeurA');
+  var valeurB = document.getElementById('valeurB');
+  var nomFonction = document.getElementById('valeur_fonction_logique');
+  valeurA.innerHTML = '<!--valeur de A-->'
+  valeurB.innerHTML = '<!--valeur de B-->'
+  nomFonction.innerHTML = '<span class="gras">Choisir une fonction logique</span>'
+
+}
+init();
